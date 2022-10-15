f01ff68360fe48256852a67c8f6f241db9cab3a4
Shell-thon <shellterminal0@gmail.com>
2022-10-15 23:54:57 +0000

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+int main() 
+{
+	//string initialisation
+	char Myword[] = "Programming is like building a multilingual puzzle";
 
+	puts(Myword); //writing the string to stdout
+
+	return 0;
+}
