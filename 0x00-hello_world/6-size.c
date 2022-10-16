742eabec017ed95d7af5b97e75ff5b5a94f5ce32
Shell-thon <shellterminal0@gmail.com>
2022-10-16 01:47:58 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@ int main(void)
 {
 int Int_type;
 long int Lon_Int_type;
-long long int simp_type;
+long long int simpe;
 float Flo_type;
 char Character_type;
 
 printf("Size char: %lu bytes \n", (unsigned long)sizeof(Character_type));
 printf("Size of int: %lu bytes \n", (unsigned long)sizeof(Int_type));
 printf("Size of long int: %lu bytes \n", (unsigned long)sizeof(Lon_Int_type));
-printf("Size of long long int: %lu bytes \n", (unsigned long)sizeof(simp_type));
+printf("Size of long long int: %lu bytes \n", (unsigned long)sizeof(simpe));
 printf("Size of float: %lu bytes \n", (unsigned long)sizeof(Flo_type));
 return (0);
 }
