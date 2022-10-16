91e6c62994aea02c07a0d2f665d9c42e056c6cfc
Shell-thon <shellterminal0@gmail.com>
2022-10-16 00:57:40 +0000

    added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+int Int_type;
+double Doub_type;
+float Flo_type;
+char Character_type;
+
+printf("Size of int: %ld bytes \n", sizeof(Int_type));
+printf("Size of double: %ld bytes \n",sizeof(Doub_type));
+printf("Size of float: %ld bytes \n",sizeof(Flo_type));
+printf("Size char: %ld bytes \n",sizeof(Character_type));
+return (0);
+}
