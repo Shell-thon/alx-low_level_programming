f9262e8eaad5e124d2496782973d7a4aee3e851c
Shell-thon <shellterminal0@gmail.com>
2022-10-16 01:24:39 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ double Doub_type;
 float Flo_type;
 char Character_type;
 
-printf("Size of int: %ld bytes \n", (unsigned long)sizeof(Int_type));
-printf("Size of Long int: %ld bytes \n", (unsigned long)sizeof(Lon_Int_type));
-printf("Size of double: %ld bytes \n", (unsigned long)sizeof(Doub_type));
-printf("Size of float: %ld bytes \n", (unsigned long)sizeof(Flo_type));
-printf("Size char: %ld bytes \n", (unsigned long)sizeof(Character_type));
+printf("Size of int: %lu bytes \n", (unsigned long)sizeof(Int_type));
+printf("Size of Long int: %lu bytes \n", (unsigned long)sizeof(Lon_Int_type));
+printf("Size of double: %lu bytes \n", (unsigned long)sizeof(Doub_type));
+printf("Size of float: %lu bytes \n", (unsigned long)sizeof(Flo_type));
+printf("Size char: %lu bytes \n", (unsigned long)sizeof(Character_type));
 return (0);
 }
