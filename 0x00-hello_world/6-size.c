0b7b21cb6502f6010559a3cdcf0b0bf7d791ee4d
Shell-thon <shellterminal0@gmail.com>
2022-10-16 02:15:32 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+
 /**
- *       main - Entry point
- *
- *         Return: Always 0 (Success)
+ *  main - Entry point
+ * 
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-int Int_type;
-long int Lon_Int_type;
-long long int simpe;
-float Flo_type;
-char Character_type;
+int a;
+long int b;
+long long int c;
+char d;
+float f;
 
-printf("Size char: %lu bytes \n", (unsigned long)sizeof(Character_type));
-printf("Size of int: %lu bytes \n", (unsigned long)sizeof(Int_type));
-printf("Size of long int: %lu bytes \n", (unsigned long)sizeof(Lon_Int_type));
-printf("Size of long long int: %lu bytes \n", (unsigned long)sizeof(simpe));
-printf("Size of float: %lu bytes \n", (unsigned long)sizeof(Flo_type));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
