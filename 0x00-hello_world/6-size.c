bc3ffa19a859d8c50c2e144f5cf7fd621d2ace18
Shell-thon <shellterminal0@gmail.com>
2022-10-16 01:12:59 +0000

    updated betty stuffs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ *       main - Entry point
+ *
+ *         Return: Always 0 (Success)
+ */
 int main(void)
 {
 int Int_type;
@@ -7,8 +12,8 @@ float Flo_type;
 char Character_type;
 
 printf("Size of int: %ld bytes \n", sizeof(Int_type));
-printf("Size of double: %ld bytes \n",sizeof(Doub_type));
-printf("Size of float: %ld bytes \n",sizeof(Flo_type));
-printf("Size char: %ld bytes \n",sizeof(Character_type));
+printf("Size of double: %ld bytes \n", sizeof(Doub_type));
+printf("Size of float: %ld bytes \n", sizeof(Flo_type));
+printf("Size char: %ld bytes \n", sizeof(Character_type));
 return (0);
 }
