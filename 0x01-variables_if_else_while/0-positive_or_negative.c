c22522d463a07c4570f5a60e1746bdb4dc39b1f6
Shell-thon <shellterminal0@gmail.com>
2022-10-16 14:02:47 -0700

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - assign a random number to int n evrytime
- * it executes, and prints it
+ * main - assign a random number to int n everytime
+ * it executes and prints it
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+	n = rand() - RAND_MAX /2;
+	if (n>0)
+		printf("%d is positve\n", n);
+	else if (n==0)
+		printf("%d is zero\n", n);
+	return (0);
 }
