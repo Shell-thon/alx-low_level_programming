c7e3d5cd5d4d0e7ab57a6699b0970e301758e3db
Shell-thon <shellterminal0@gmail.com>
2022-10-16 14:52:19 -0700

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  *  main - Entry point
  *
@@ -8,9 +8,24 @@
  */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-return (0);
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			printf("%d is negative\n", n);
+		}
+		else
+		{
+			printf("%d is positive\n", n);
+		}
+	}
+	/* your code goes there */
+	return (0);
 }
