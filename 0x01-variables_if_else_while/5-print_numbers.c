a4ff8332dd119be9ce0a70463778736c6cf9aed5
Shell-thon <shellterminal0@gmail.com>
2022-10-17 00:15:37 -0700

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *    main - Entry point
+ *
+ *    Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char n1 = \n;
+
+	for (c = 0; c <= '9'; ++c)
+	{
+		putchar(c);
+	}
+	putchar(n1);
+
+	return (0);
+}
