e76ff917d8142521737ce67479773455117ce17d
Shell-thon <shellterminal0@gmail.com>
2022-10-16 23:58:47 -0700

    testing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c;
+	char n1 =\n;
+
+	for (c = 'a; c<=z; ++c)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar(n1);
+
+	return (0);
+}
