f18afc2030cd1c48561ef77e63cfe782ce2de1dd
Shell-thon <shellterminal0@gmail.com>
2022-10-17 00:23:10 -0700

    fixed 4 syntax error using the hel of betty

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,14 +3,13 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
 	char c;
-	char n1 = \n;
+	char n1 = '\n';
 
-	for (c = 'a; c<=z; ++c)
+	for (c = 'a'; c <= 'z'; ++c)
 	{
 		if (c != 'e' && c != 'q')
 		{
