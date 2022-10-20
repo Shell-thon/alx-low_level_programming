81895a2db6b7b95885c9472f1889b8168186770e
Shell-thon <shellterminal0@gmail.com>
2022-10-20 04:32:51 +0000

    added  10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  add - function
+ *  @a: first operand
+ *  @b: second operand
+ *
+ * Description: function that adds two integers and returns the result.
+ * Return: Always 0 (success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
