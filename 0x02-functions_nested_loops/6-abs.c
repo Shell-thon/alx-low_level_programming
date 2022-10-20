4e6376824637febb7925af1f7e5348128a1a3070
Shell-thon <shellterminal0@gmail.com>
2022-10-20 02:07:04 +0000

    added 6-abs.c perfectly

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - function
+ * @r: Frist operand:
+ *
+ * Description: computes the absolute value of am imteger.
+ * Return: Always 0 (Success)
+ */
+int _abs(int r)
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+	return (0);
+}
