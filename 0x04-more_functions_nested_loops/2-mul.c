43d97be941df8f2ca354768245b2be71b9c02461
Shell-thon <shellterminal0@gmail.com>
2022-10-21 06:24:35 -0700

    #include "holberton.h"

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* mul - function
+* @a: first operand
+* @b: second operand
+*
+* Decription: Multiply two integers.
+* Return: Always 0(success)
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
