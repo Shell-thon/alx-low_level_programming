e83110458f3bfc00f28d874f5ebf59886710974c
Shell-thon <shellterminal0@gmail.com>
2022-10-21 06:13:29 +0000

    still fixing some errors in file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,13 @@
  * main -test function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
+return (0);
 }
