c7f7fee364dd422b6eaaee02494ed5933af68bb6
Shell-thon <shellterminal0@gmail.com>
2022-10-21 14:20:12 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.c"
+/**
+* print_square - function
+* @size: first operand
+*
+* Description: prints a square, followed by a new line
+* Return: Always 0 (success)
+*/
+void print_square(int size)
+{
+	int i;
+	int d;
+
+	if (size <= 0)
+		_putchar(10);
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (d = 1; d < size; d++)
+			{
+				_putchar('#');
+			}
+			_putchar('#');
+			_putchar(10);
+		}
+	}
+}
