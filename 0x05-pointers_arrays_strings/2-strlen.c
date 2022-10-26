236458278f8b0c273170d3d67a90cd3627b8765f
Shell-thon <shellterminal0@gmail.com>
2022-10-25 17:05:54 -0700

    edited 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: Type char
+ * Return: a
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+
+	}
+	return (a);
+}
