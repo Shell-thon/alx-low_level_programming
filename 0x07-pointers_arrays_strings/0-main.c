5307dd44829d9e4cd13e6d1d26d74165cec5b3b6
Shell-thon <shellterminal0@gmail.com>
2022-11-02 16:00:03 +0100

    update

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -10,32 +10,29 @@
  */
  void simple_print_buffer(char *buffer, unsigned int size)
  {
-         unsigned int i;
+	 unsigned int i;
 
-         i = 0;
-         while (i < size)
-         {
-                 if (i % 10)
-                 {
-                         printf(" ");
-                 }
-                 if (!(i % 10) && i)
-                 {
-                         printf("\n");
-                 }
-                 printf("0x%02x", buffer[i]);
-                 i++;
-         }
-         printf("\n");
- }
-
+	 i = 0;
+	 while (i < size)
+	 {
+		 if (i % 10)
+		 {
+			 printf(" ");
+		 }
+		 if (!(i % 10)  && i)
+		 {
+			 printf("\n");
+		 }
+		 printf("0x%02x", buffer0[i]);
+		 i++;
+	 }
  /**
   * main - check the code for Holberton School students.
   *
   * Return: Always 0.
   */
- int main(void)
- {
+int main(void)
+{
          char buffer[98] = {0x00};
 
          simple_print_buffer(buffer, 98);
@@ -43,4 +40,4 @@
          printf("-------------------------------------------------\n");
          simple_print_buffer(buffer, 98);
          return (0);
- }
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -11,6 +10,4 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif
-
