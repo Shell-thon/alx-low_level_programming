a579bb99ec801ff128d12aa7c397520224954d46
Shell-thon <shellterminal0@gmail.com>
2022-11-02 12:01:32 +0100

    update

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-  * simple_print_buffer - prints buffer in hexa
-  * @buffer: the address of memory to print
-  * @size: the size of the memory to print
-  *
-  * Return: Nothing.
-  */
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
  void simple_print_buffer(char *buffer, unsigned int size)
  {
          unsigned int i;
@@ -40,6 +41,6 @@
          simple_print_buffer(buffer, 98);
          _memset(buffer, 0x01, 95);
          printf("-------------------------------------------------\n");
-         simple_print_buffer(buffer, 98);    
+         simple_print_buffer(buffer, 98);
          return (0);
  }
