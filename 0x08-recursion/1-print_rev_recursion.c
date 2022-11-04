eb779a2a50ebb8beb13df3025a38071ade481365
Shell-thon <shellterminal0@gmail.com>
2022-11-04 06:45:32 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  _print_rev_recursion - print a string in reverse
+ *
+ *   @s: pointer of char type
+ *
+ *   Return: reversed
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+		if (*s)
+
+		{
+
+			_print_rev_recursion(s + 1);
+
+			_putchar(*s);
+
+		}
+
+}
