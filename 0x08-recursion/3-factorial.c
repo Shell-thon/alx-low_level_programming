fc50b92dd5977af4f17f30bf3820ee2326069dcc
Shell-thon <shellterminal0@gmail.com>
2022-11-04 07:00:25 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ *
+ *   @n: int type
+ *
+ *   Return: returns -1 if number less than zero, else return factorial
+ *
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+
+	return (-1);
+
+	if (n >= 1)
+
+	return (n * factorial(n - 1));
+
+	else
+
+	return (1);
+
+}
