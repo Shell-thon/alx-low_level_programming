f0ae4b3113ec7f2c0ed5ce61eea022ef61f723e6
Shell-thon <shellterminal0@gmail.com>
2022-11-04 06:54:13 -0700

    added 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  _strlen_recursion - find length of a string
+ *
+ *  @s: pointer og char type
+ *
+ *  Return: return
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+
+	{
+
+		return (0);
+
+	}
+
+	else
+
+	{
+
+		return (1 + _strlen_recursion(s + 1));
+
+	}
+
+}
