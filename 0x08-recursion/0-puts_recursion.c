6369dac2660a7c22b645dcbb386ce657817e188e
Shell-thon <shellterminal0@gmail.com>
2022-11-04 06:37:01 -0700

    added 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *  _puts_recursion - function that prints a string
+ *
+ *  @s: pointer of char type
+ *
+ *  Return: Always success
+ *
+ */
+
+void _puts_recursion(char *s)
+
+{
+
+		if (*s == '\0')
+		{
+
+			_putchar('\n');
+
+			return;
+
+		}
+
+		else
+
+		{
+
+			_putchar(*s);
+
+			_puts_recursion(s + 1);
+
+		}
+
+}
