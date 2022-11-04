fce2a7561909516b87d08d79b8d6d3fe16dc2466
Shell-thon <shellterminal0@gmail.com>
2022-11-03 23:08:54 -0700

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,16 +1,16 @@
 #include "main.h"
-<<<<<<< HEAD
 #define NULL 0
 
 /**
-* _strspn - function
-* @s: return num bytes of
-* @accept: bytes from
+*  _strspn - function
+*  @s: the firat parameter s return num bytes of
+*  @accept: the second parameter *accept bytes from
 *
-* Description: function to get through length of prefix substring
+* Description: _strspn function to get through 
+* length of prefix substring
 * Return: s
 */
-unsigned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 		int i, j, counter;
 
@@ -35,35 +35,4 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 		return (counter);
-=======
-
-/**
- * _strspn - gets the length of prefix substring
- * @s: The string to be searched
- * @accept: The prefix to be measured.
- *
- * Return: The number of bytes in s which
- * consists only of bytes from accept.
- */
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int bytes = 0;
-	int index;
-
-	while (*s)
-	{
-		for (index = 0; accept[index];index++)
-		{
-			if (*s == accept[index])
-			{
-				bytes++;
-				break;
-			}
-			else if(accept[index +1] == '\0')
-				return (bytes);
-		}
-		s++
-	}
-	return (bytes);
->>>>>>> 5a8ecebf776363e64ced70bd210c11116c4b09c0
 }
