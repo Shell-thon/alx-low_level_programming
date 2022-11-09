0b5b92540eb6177d81ba10bec9de85601d1778da
Shell-thon <shellterminal0@gmail.com>
2022-11-08 16:33:20 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+int check_change(int money);
+int is_number(char *s);
 /**
  * main - print the mun of coins
  * @argc: int type
@@ -8,37 +11,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int coins = 0;
-	int cents;
-
-	if (argc ==  1)
+	if (argc < 2 && argc <= 1)
 	{
 		printf("Error\n");
 		return (1);
-	}
-	else if (argc > 1)
-	{
-		cents = atoi(argv[1]);
-		if (cents >= 25)
-		{
-			coins += cents / 25;
-			cents %= 25;
-		}
-		if (cents >= 5)
-		{
-			coins += cents / 5;
-			cents %= 5;
-		}
-		if (cents >= 2)
-		{
-			coins += cents / 2;
-			cents %= 2;
-		}
-		if (cents >= 1)
-		{
-			coins += cents / 1;
-		}
-		printf("%d\n", coins);
 	}
 	return (0);
 }
