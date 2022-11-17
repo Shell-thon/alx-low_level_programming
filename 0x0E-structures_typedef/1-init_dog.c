348a47938b95e6a3c6577bb10e9f8c56bec95e27
Shell-thon <shellterminal0@gmail.com>
2022-11-16 19:37:54 -0800

    init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize the struct dog
+ * @d: pointer to dog struct
+ * @name: char type
+ * @age: float type
+ * @owner: char type
+ * Return: return pointer to struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
