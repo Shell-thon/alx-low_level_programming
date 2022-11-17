99bf955f306edc4db8a62551ebaf5fbd949c960a
Shell-thon <shellterminal0@gmail.com>
2022-11-16 20:03:52 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,22 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - struct for dog info
  * @name: type char
  * @age: float type
  * @owner: char type
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
 
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+#endif /* DOG_H */
