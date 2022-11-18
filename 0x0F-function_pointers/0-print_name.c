9c0367c097401fbfbcbde40ed6ec0d58cb16aeed
Shell-thon <shellterminal0@gmail.com>
2022-11-18 03:02:49 -0800

    added 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,16 @@
 #include "function_pointers.h"
 /**
- *  print_name - Print a name
- *  @name: Pointer of char type
- *  @f: Pointer to function
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
+
+	if (name && f)
+
 		f(name);
+
 }
