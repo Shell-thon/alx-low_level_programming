d70ee6d0176e9b2ae4ff07be3f4d74a9fb48ed6b
Shell-thon <shellterminal0@gmail.com>
2022-11-18 13:27:36 +0000

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,28 +11,18 @@
  * Description: If separator is NULL, it is not printed.
  *              If one of the strings if NULL, (nil) is printed instead.
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list strings;
-	char *str;
-	unsigned int index;
+	va_list valist;
+	unsigned int i;
 
-	va_start(strings, n);
-
-	for (index = 0; index < n; index++)
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strings, char *);
-
-		if (str == NULL)
-			printf("(nil)");
-		else
-			printf("%s", str);
-
-		if (index != (n - 1) && separator != NULL)
+		printf("%d",  va_arg(valist, int));
+		if (separator && (i < (n - 1)))
 			printf("%s", separator);
 	}
-
 	printf("\n");
-
-	va_end(strings);
+	va_end(valist);
 }
