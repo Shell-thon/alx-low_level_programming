29945c7991177fc29b50722fa10fb32be8b0849e
Shell-thon <shellterminal0@gmail.com>
2022-11-29 00:30:15 +0000

    update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *  print_listint - function
+ *  @h: string to print
+ *
+ *  Description: Fnction to print all elements in listint_t
+ *  Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int elements = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
