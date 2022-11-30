f7691afa3652848af5005f8001a1960407b041a3
Shell-thon <shellterminal0@gmail.com>
2022-11-30 01:45:39 +0000

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,38 +10,37 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode, *temp;
-	unsigned int num;
+	unsigned int count = 0;
 
-	if (head == NULL)
+	listint_t *new;
+	listint_t *temp;
+
+	if (*head == NULL)
 		return (NULL);
 
-	newNode = (listint_t *)malloc(sizeof(listint_t));
-	if (newNode == NULL)
-		return (NULL);
-
-	newNode->n = n;
-	newNode->next = NULL;
-
-	if (idx == 0)
-	{
-		newNode->next = (*head);
-		(*head) = newNode;
-		return (newNode);
-	}
 	temp = *head;
 
-
-	for (num = 2; num <= idx; num++)
+	while (temp != NULL)
 	{
+		if (count == idx - 1)
+		{
+			new = malloc(sizeof(listint_t));
+			if (new == NULL)
+			{
+				return (NULL);
+			}
+			else
+			{
+				new->n = n;
+				new->next = temp->next;
+				temp->next = new;
+				return (new);
+			}
 
-		if (temp == NULL)
-			return (NULL);
-
-		temp = temp->next;
+		}
+		if (temp->next != NULL)
+			temp = temp->next;
+		count++;
 	}
-
-	newNode->next = temp->next;
-	temp->next = newNode;
-	return (newNode);
+	return (NULL);
 }
