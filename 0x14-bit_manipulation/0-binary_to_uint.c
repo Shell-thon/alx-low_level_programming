22815b069cf457723dfd15619881820e89dd3d09
Shell-thon <shellterminal0@gmail.com>
2022-12-02 19:57:36 -0800

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,27 +6,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int r = 0;
-	unsigned int i = 0;
+	unsigned int num;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i] != '\0')
+	for (num = 0; *b; b++)
 	{
-		if (b[i] == '1')
-		{
-			r = (r * 2) + 1;
-			i++;
-		}
-		else if (b[i] == '\0')
-		{
-			r = (r * 2);
-			i++
-		}
+		if (*b == '1')
+			num = (num << 1) | 1;
+		else if (*b == '0')
+			num = num << 1;
 		else
 			return (0);
 	}
-
-	return (r);
+	return (num);
 }
