460123fe9d549d661fa03650fc496d478ecea98e
Shell-thon <shellterminal0@gmail.com>
2022-12-06 02:18:30 +0000

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int length = 0, inlen = 0;
-	char *ptr;
+	 int fd, sz = 0, wr;
 
-	if (filename == NULL)
+	 if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[sz] != '\0')
+		sz++;
+
+	wr = write(fd, text_content, sz);
+
+	if (wr == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (inlen = 0, ptr = text_content; *ptr; ptr++)
-			inlent++
-		length = write(file, text_content, inlen);
-	}
-
-	if (close(file) == -1 || inlen != length)
-		return (-1)
+	close(fd);
 	return (1);
 }
