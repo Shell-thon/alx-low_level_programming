15eab7c7b3a9ac9feff1500b7f89781d75b61ad4
Shell-thon <shellterminal0@gmail.com>
2023-01-21 04:20:04 +0100

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,20 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings.
+ * _strncat - C function that copies a string.
  * @dest: Type char
  * @src: Type char
  * @n: Type int
  *
  * Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int a, b;
+int a, b;
 
-	for (a = 0; dest[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; b < n && src[b] != '\0'; b++)
-	{
-		dest[a + b] = src[b];
-	}
-	return (dest);
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
 }
