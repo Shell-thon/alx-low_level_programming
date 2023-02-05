5395e76a8b43821b8999df81f411a47e8edef247
Shell-thon <shellterminal0@gmail.com>
2023-02-05 02:20:57 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,27 +10,28 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table;
-  unsigned long int i;
+hash_table_t *new_table;
+unsigned long int i;
 
-  if (size < 1)
-    return (NULL);
+if (size < 1)
+return (NULL);
 
-  new_table = malloc(sizeof(hash_table_t));
-  if (new_table == NULL)
-    return (NULL);
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
 
-  new_table->size = size;
+new_table->size = size;
 
-  new_table->array = malloc(sizeof(hash_node_t *) * size);
-  if (new_table->array == NULL)
-    {
-      free(new_table);
-      return (NULL);
-    }
+new_table->array = malloc(sizeof(hash_node_t *) * size);
 
-  for (i = 0; i < size; i++)
-    new_table->array[i] = NULL;
+if (new_table->array == NULL)
+{
+free(new_table);
+return (NULL);
+}
 
-  return (new_table);
+for (i = 0; i < size; i++)
+new_table->array[i] = NULL;
+
+return (new_table);
 }
