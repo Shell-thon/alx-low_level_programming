fd0fb46a71634da7598e37e76b7706562bc28e4b
Shell-thon <shellterminal0@gmail.com>
2023-07-04 01:49:26 +0100

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * wildcmp - Compares two strings considering the '*' wildcard.
@@ -9,21 +9,21 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*')
-            return wildcmp(s1, s2 + 1);
-        else if (*(s2 + 1) == '\0')
-            return 1;
-        else if (*s1 == '\0')
-            return wildcmp(s1, s2 + 1);
-        else
-            return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
-    }
-    else if (*s1 != *s2)
-        return 0;
-    else if (*s1 == '\0')
-        return 1;
-    else
-        return wildcmp(s1 + 1, s2 + 1);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		else if (*(s2 + 1) == '\0')
+			return (1);
+		else if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		else
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	else if (*s1 != *s2)
+		return (0);
+	else if (*s1 == '\0')
+		return (1);
+	else
+		return (wildcmp(s1 + 1, s2 + 1));
 }
